b16c8a2f44fa678171aeaafedbf743590b5f6c7f
Spartan17Guy <hounyojo@msu.edu>
2016-11-02 12:11:30 -0400

    Obtain datetime attributes
    Next step, save the processed file as numpy object for quick loading

---
+++ preprocessing.py
@@ -0,0 +1,22 @@
+"""
+This is the first python file to obtain days and months
+"""
+
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+# this function is useless
+def formaldate(string):
+	item = string.split("/") # this is the date components
+	return dt.date(int(item[2]), int(item[1]), int(item[0])) # so return the date as datetime in the dataframe
+# end of useless function
+	
+# use pd.timestamp() instead. weekday: Mon-Sun: 0-6
+
+df = pd.read_csv('NIJ2016_JAN01_JUL31.csv')
+# find out if any dates are missing
+df['FormalDate'] = pd.to_datetime(df.occ_date)
+df['Weekday'] = pd.Series(map(lambda x: x.weekday(), df.FormalDate)) # Mon-Sun : 0-6
+
+# now this can be grouped and manipulated per variable ...
